c05cc3faac26c9dd0841d0f019a442aab710bf7a
Milton Pena <milton@sonargroup.com.au>
2025-12-05 09:55:15 +1100

    Update separator block

--- web/app/themes/kleanhub/resources/views/blocks/separator.blade.php
+++ web/app/themes/kleanhub/resources/views/blocks/separator.blade.php
@@ -7,4 +7,8 @@
   Preview: separator
   Icon: minus
 --}}
-<section id="{{ $block_id }}" class="block separator {{ $block_class }}"></section>
+<section id="{{ $block_id }}" class="block separator px-5 {{ $block_class }}">
+  <div class="container mx-auto max-w-[83.125rem]">
+    <div class="bg-blue w-full h-px"></div>
+  </div>
+</section>
